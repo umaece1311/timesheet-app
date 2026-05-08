8f5f4dc57d52fe1b9d736c1162c644da2fc13a89
umaece1311@gmail.com <umaece1311@gmail.com.>
2026-05-08 12:12:59 +0200

    Fix GitHub Pages base path and deploy workflow

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/timesheet-app">
       <AuthProvider>
         <App />
       </AuthProvider>
