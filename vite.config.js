b257a999b56979c8c778d294d94178c37e75d4c6
umaece1311@gmail.com <umaece1311@gmail.com.>
2026-05-08 12:03:08 +0200

    Setup GitHub Pages deployment with GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/timesheet-app/',
   plugins: [react()],
   server: {
     host: true,        // expose on LAN so phone can connect during dev
